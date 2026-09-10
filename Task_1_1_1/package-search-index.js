5ddccabab9d642d8c09885476558c48708739f52
nata-medvedeva <nata-medvedeva@users.noreply.github.com>
2026-09-10 06:09:26 +0000

    Deploying to gh-pages from @ nata-medvedeva/OOP@5479eb271b3588f0e59ecdc3dfe61320d3b1495a 🚀

--- Task_1_1_1/package-search-index.js
+++ Task_1_1_1/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.nmedvedeva1.sort"},{"l":"ru.nsu.shadrina"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.nmedvedeva1.sort"}];updateSearchResults();
